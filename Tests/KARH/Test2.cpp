b5b95443dcf3bd5f7e07da6f1129021bf77ee9a9
Godfather22 <hristo2766@gmail.com>
2019-04-06 00:56:15 +0300

    Remove shlokavica

--- Tests/KARH/Test2.cpp
+++ Tests/KARH/Test2.cpp
@@ -67,20 +67,22 @@ int Question::AnswerQuestion()
 		std::cin >> tmp;
 		tmpToInt = tmp;
 	} while (tmp != 's' && tmp != 'S');
-	std::cout << "Rezultat: " << m_Score << "\nVeren otgovor(i): ";
+	std::cout << "Резултат: " << m_Score << "\nВерен отговор(и): ";
 	for (unsigned i = 0; i < m_Answers.size(); ++i)
 		if (m_Answers[i].val > 0)
 			std::cout << char(i + 65) << ' ';
-	std::cout << "\nVavedi nqkoi simvol";
+	std::cout << "\nВъведи някой символ...\n";
 	if (m_Score > 0)
 		counter++;
 	return m_Score;
 }
 int main()
 {
-	std::cout << "Test 2 po KARH\nIzgotven ot Mapmo, Dakata, Ikbal i M.\n";
-	std::cout << "Za da otgovorite na nqkoi vupros vavedete bukvata(bukvite) i kogato ste gotovi vavedete 's'\n";
-	std::cout << "Suobrazete se kak raboti std::cin\nAko jelaete simuaciq bez povtorni opiti natisnete r\nAko jelaete vuprosite, koito oburkate da se zapazvat natisnete proizvolen buton\n";
+	setlocale(LC_ALL, "Bulgarian");
+
+	std::cout << "Тест 2 по КАРХ.\nИзготвен от Марто, Даката, Икбал и М.\nПреведен от shlokavica на български от Ицо.\n";
+	std::cout << "За да отговорите на някой въпрос, въведете буквата (буквите) и когато сте готови въведете 's'.\n";
+	std::cout << "Съобразете се как работи std::cin.\nАко желаете симулация без повторни опити, натиснете r.\nАко желаете въпросите, които объркате да се запазват, натиснете произволен бутон.\n";
 	char useless;
 	std::cin >> useless;
 	bool flag = (useless == 'r' ? 1 : 0);
@@ -88,21 +90,21 @@ int main()
 	std::vector<Question> exam;
 	std::srand(unsigned(std::time(0)));
 
-	Question q1("Kakva e celta na izpolzvaneto na CMOVZ?");
-	q1.AddOption("Reducirane na uuslovnite prehodi", 1);
-	q1.AddOption("Spestqvane na pamet", -1);
-	q1.AddOption("Izbqgvane na logicheska greshka", -1);
-	q1.AddOption("Predotvratqvane na mehuri v konveyera", -1);
+	Question q1("Каква е целта на използването на CMOVZ?");
+	q1.AddOption("Редуциране на условните преходи", 1);
+	q1.AddOption("Спестяване на памет", -1);
+	q1.AddOption("Избягване на логическа грешка", -1);
+	q1.AddOption("Предотвратяване на мехури в конвейера", -1);
 	exam.push_back(q1);
 
-	Question q2("Koi ot izbroenite abreviaturi sa imena na shini?");
+	Question q2("Кои от изброените абревиатури са имена на шини?");
 	q2.AddOption("USB, CAN, I2C", 1);
 	q2.AddOption("LPT, PWM, I2C", -1);
 	q2.AddOption("Com-port, PCA, PWM", -1);
 	q2.AddOption("PCA, ISA, RJ11", -1);
 	exam.push_back(q2);
 
-	Question q3("Koi ot posochenite komponenti osushtestvqva vruzka s vutreshna shina?");
+	Question q3("Кой от посочените компоненти осъществява връзка с вътрешна шина?");
 	q3.AddOption("USB", -1);
 	q3.AddOption("LPT", -1);
 	q3.AddOption("Com-port", -1);
@@ -111,7 +113,7 @@ int main()
 	q3.AddOption("Ethernet", -1);
 	exam.push_back(q3);
 
-	Question q4("Koi ot posochenite komponenti osushtestvqva vruzka s vunshna shina?");
+	Question q4("Кой от посочените компоненти осъществява връзка с външна шина?");
 	q4.AddOption("USB", 0.25);
 	q4.AddOption("LPT", 0.25);
 	q4.AddOption("Com-port", 0.25);
@@ -120,123 +122,123 @@ int main()
 	q4.AddOption("Ethernet", 0.25);
 	exam.push_back(q4);
 
-	Question q5("Kak se sluchvat vav vremeto REQ i GNT?");
-	q5.AddOption("Poluchava se GNT, a sled tova REQ", -5);
-	q5.AddOption("Poluchava se REQ, sled koeto GNT", 1);
-	q5.AddOption("Sled poluchavane na REQ, vednaga se generiran GNT", -1);
-	q5.AddOption("Sled poluchavane na GNT, vednaga se generira REQ", -5);
+	Question q5("Как се случват във времето REQ и GNT?");
+	q5.AddOption("Получава се GNT, а след това REQ", -5);
+	q5.AddOption("Получава се REQ, след което GNT", 1);
+	q5.AddOption("След получаване на REQ, веднага се генерира GNT", -1);
+	q5.AddOption("След получаване на GNT, веднага се генерира REQ", -5);
 	exam.push_back(q5);
 
-	Question q6("Mejdu T3 i T4 koy ustanovqva signalite za danni na shinata za purvata chast na kartinkata?\nIMG not found");
+	Question q6("Между T3 и T4 кой установява сигналите за данни на шината за първата част на картинката?\nIMG not found");
 	q6.AddOption("Master", -1);
 	q6.AddOption("Slave", 1);
 	exam.push_back(q6);
-	
-	Question q7("Mejdu T3 i T4 koy ustanovqva signalite za danni na shinata za vtorata chast na kartinkata?\nIMG not found");
+
+	Question q7("Между T3 и T4 кой установява сигналите за данни на шината за втората част на картинката?\nIMG not found");
 	q7.AddOption("Master", 1);
 	q7.AddOption("Slave", -1);
 	exam.push_back(q7);
 
-	Question q8("Zashto ima po edna strelka vuv vsqka posoka?");
-	q8.AddOption("Zashtoto sa diferencialni dvoyki", -1);
-	q8.AddOption("Zashtoto ne stiga chestotnata liniq", -1);
-	q8.AddOption("Za postigane na po-visoki skorosti", -1);
-	q8.AddOption("Vsichki sa verni", 1);
+	Question q8("Защо има по една стрелка във всяка посока?");
+	q8.AddOption("Защото са диференциални двойки", -1);
+	q8.AddOption("Защото не стига честотната линия", -1);
+	q8.AddOption("За постигане на по-високи скорости", -1);
+	q8.AddOption("Всички са верни", 1);
 	exam.push_back(q8);
 
-	Question q9("Kakvo oznachava CRC?");
+	Question q9("Какво означава CRC?");
 	q9.AddOption("Cyclic Redundancy Check", 1);
 	q9.AddOption("Complete Register Content", -1);
 	q9.AddOption("Connection Regulating Chip", -1);
 	q9.AddOption("Create Read Clear", -1);
 	exam.push_back(q9);
 
-	Question q10("Koq shina ima posoka ot uC HOST kum Ethernet?\nImg not found");
+	Question q10("Коя шина има посока от uC HOST към Ethernet?\nImg not found");
 	q10.AddOption("TXD", 1);
 	q10.AddOption("RXD", -1);
 	exam.push_back(q10);
 
-	Question q11("Koq shina ima posoka ot Ethernet kum ucHost?\nImg not found");
+	Question q11("Коя шина има посока от Ethernet към ucHost?\nImg not found");
 	q11.AddOption("TXD", -1);
 	q11.AddOption("RXD", 1);
 	exam.push_back(q11);
 
-	Question q12("Kakvo e DMA?");
+	Question q12("Какво е DMA?");
 	q12.AddOption("Direct Memory Access", 1);
 	q12.AddOption("Digital Machine Accumulator", -1);
 	q12.AddOption("Digital Memory Access", -1);
 	q12.AddOption("Detector of Masked Attacks", -1);
 	exam.push_back(q12);
 
-	Question q13("Kolko provodna e USB3 shinata?");
+	Question q13("Колко проводна е USB3 шината?");
 	q13.AddOption("9", 1);
 	q13.AddOption("3", -1);
 	q13.AddOption("6", -1);
 	q13.AddOption("1", -1);
 	exam.push_back(q13);
 
-	Question q14("Kolko provodna e USB2 shinata?");
+	Question q14("Колко проводна е USB2 шината?");
 	q14.AddOption("4", 1);
 	q14.AddOption("2", -1);
 	q14.AddOption("8", -1);
 	q14.AddOption("1", -1);
 	exam.push_back(q14);
 
-	Question q15("Zashto D+ i D- sa usukani?");
-	q15.AddOption("Za povishavane na skorostta", -1);
-	q15.AddOption("Za po-golqma zdravina", -1);
-	q15.AddOption("Za po-dobra guvkavost na kabela", -1);
-	q15.AddOption("Za shumoustoychivost", 1);
+	Question q15("Защо D+ и D- са усукани?");
+	q15.AddOption("За повишаване на скоростта", -1);
+	q15.AddOption("За по-голяма здравина", -1);
+	q15.AddOption("За по-добра гъвкавост на кабела", -1);
+	q15.AddOption("За шумоустойчивост", 1);
 	exam.push_back(q15);
 
-	Question q16("Kakvo e SOF?");
+	Question q16("Какво е SOF?");
 	q16.AddOption("Start of Frame", 1);
 	q16.AddOption("Superior orbital fissure", -100);
 	q16.AddOption("Sequence of Files", -1);
 	q16.AddOption("Single Offset Flag", -1);
 	exam.push_back(q16);
 
-	Question q17("Koy freim e za chetene?");
-	q17.AddOption("Purvi", 1);
-	q17.AddOption("Vtori", -1);
-	q17.AddOption("Treti", -1);
-	q17.AddOption("Chetvrti", -1);
+	Question q17("Кой фрейм е за четене?");
+	q17.AddOption("Първи", 1);
+	q17.AddOption("Втори", -1);
+	q17.AddOption("Трети", -1);
+	q17.AddOption("Четвърти", -1);
 	exam.push_back(q17);
 
-	Question q18("Koy freim e za pisane?");
-	q18.AddOption("Purvi", -1);
-	q18.AddOption("Vtori", -1);
-	q18.AddOption("Treti", 1);
-	q18.AddOption("Chetvrti", -1);
+	Question q18("Кой фрейм е за писане?");
+	q18.AddOption("Първи", -1);
+	q18.AddOption("Втори", -1);
+	q18.AddOption("Трети", 1);
+	q18.AddOption("Четвърти", -1);
 	exam.push_back(q18);
 
-	Question q19("Za kakvo slujat rezistorite?");
-	q19.AddOption("Za suzdavane na elektrichesko suprotivlenie", 1);
-	q19.AddOption("Za po-burza vruzka s pametta", -1);
-	q19.AddOption("Za zashtita ot udari", -1);
-	q19.AddOption("Za uskorqvane na vruzkata mejdu shinite i procesora", -1);
+	Question q19("За какво служат резисторите?");
+	q19.AddOption("За създаване на електрическо съпротивление", 1);
+	q19.AddOption("За по-бърза връзка с паметта", -1);
+	q19.AddOption("За защита от удари", -1);
+	q19.AddOption("За ускоряване на връзката между шините и процесора", -1);
 	exam.push_back(q19);
 
-	Question q20("Kakvo e prednaznachenieto na predicate?");
-	q20.AddOption("Da opishe daden proces", 1);
-	q20.AddOption("Da optimizira operaciq", -1);
-	q20.AddOption("Da namali latentnostta", -1);
-	q20.AddOption("Da uskori rabotata s procesora", -1);
+	Question q20("Какво е предназначението на predicate?");
+	q20.AddOption("Да опише даден процес", 1);
+	q20.AddOption("Да оптимизира операция", -1);
+	q20.AddOption("Да намали латентността", -1);
+	q20.AddOption("Да ускори работата с процесора", -1);
 	exam.push_back(q20);
 
-	Question q21("Ima srqzana kartinka i Rusalcho pita koe e pyrvo");
+	Question q21("Има срязана картинка и Русалчо пита кое е първо");
 	q21.AddOption("Read", 1);
 	q21.AddOption("Write", -1);
 	q21.AddOption("Idle", -1);
 	exam.push_back(q21);
 
-	Question q22("Ima srqzana kartinka i Rusalcho pita koe e po sredata");
+	Question q22("Има срязана картинка и Русалчо пита кое е по средата");
 	q22.AddOption("Read", -1);
 	q22.AddOption("Write", -1);
 	q22.AddOption("Idle", 1);
 	exam.push_back(q22);
 
-	Question q23("Ima srqzana kartinka i Rusalcho pita koe e nakraq");
+	Question q23("Има срязана картинка и Русалчо пита кое е накрая");
 	q23.AddOption("Read", -1);
 	q23.AddOption("Write", 1);
 	q23.AddOption("Idle", -1);
@@ -249,7 +251,7 @@ int main()
 	{
 		if (i == questionsCount)
 		{
-			std::cout << "Sekciq sus sgresheni vuprosi\nnatisnete nqkoy buton\n";
+			std::cout << "Секция със сгрешени въпроси\nнатиснете някой бутон\n";
 			std::cin >> useless;
 			system("CLS");
 		}
